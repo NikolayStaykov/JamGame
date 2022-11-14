ee489477d4537b92e44885bd7b8a4e90fbc24c7b
NikolayStaykov <nikolay.staykov98@abv.bg>
2022-11-14 11:09:24 +0200

    fixed some issues with the implementation of the targetting laser

--- RedSnow/Assets/Scripts/WeaponScripts/IWeapon.cs
+++ RedSnow/Assets/Scripts/WeaponScripts/IWeapon.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public abstract class IWeapon : MonoBehaviour
 {
-    public string laserSightTag;
+    public Transform laserDirection;
     public Transform laserOrigin;
     private LineRenderer laserSight;
     private RaycastHit lasersightHit;
@@ -16,9 +16,18 @@ public abstract class IWeapon : MonoBehaviour
 
     public void renderLaser()
     {
-        Physics.Raycast(laserOrigin.position, laserOrigin.forward, out lasersightHit,1000);
-        laserSight.SetPosition(0,laserOrigin.position);
-        laserSight.SetPosition(1, lasersightHit.point);
+        laserSight.SetPosition(0, laserOrigin.position);
+        if (Physics.Raycast(laserOrigin.position, laserOrigin.forward, out lasersightHit,1000))
+        {
+            laserSight.SetPosition(1, lasersightHit.point);
+        }
+        else
+        {
+            laserSight.SetPosition(1, laserDirection.position);
+        }
+        Debug.Log("Origin"+laserOrigin.position.ToString());
+        Debug.Log("ray hit"+lasersightHit.point.ToString());
+        Debug.Log("forward"+laserOrigin.forward.ToString());
     }
 
     public abstract void fireWeapon();
